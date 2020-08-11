a915a167e0be2bb23c7321430348d1d346688a12
James Sterling <james.sterling@easci.com>
2020-08-10 19:18:41 -0700

    docker stuff

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-# FROM alpine:3.7
-
 FROM node:12
+# FROM node:12-slim
+# FROM blueimp/nightwatch
 
 # Create app directory
 WORKDIR /usr/src/app
@@ -10,9 +10,21 @@ WORKDIR /usr/src/app
 # where available (npm@5+)
 COPY package*.json ./
 
-RUN npm install
+# fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst fonts-freefont-ttf 
+RUN apt-get update \
+    && apt-get install -y wget gnupg \
+    && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
+    && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
+    && apt-get update \
+    && apt-get install -y google-chrome-stable libxss1 \
+      --no-install-recommends \
+    && rm -rf /var/lib/apt/lists/*
+
+RUN npm install --only=production
 # If you are building your code for production
 # RUN npm ci --only=production
 
 # Bundle app source
-COPY . .
+COPY . ./
+
+CMD [ "npm", "run", "slack" ]
--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -11,7 +11,8 @@ module.exports = {
 
   webdriver: {
     start_process: true,
-    server_path: (os.platform() == 'win32') ? "node_modules/chromedriver/lib/chromedriver/chromedriver.exe" : "node_modules/.bin/chromedriver",
+    server_path: "node_modules/.bin/chromedriver",
+    // server_path: "node_modules/chromedriver/lib/chromedriver/chromedriver",
     port: 9515
   },
 
@@ -21,7 +22,11 @@ module.exports = {
         browserName: "chrome"
       },
       launch_url: `https://washington.goingtocamp.com`,
+
+      // Lake Wenatchee
       site_uri: `/create-booking/results?mapId=-2147483375&searchTabGroupId=0&bookingCategoryId=0&startDate=2020-08-21T00:00:00.000Z&endDate=2020-08-23T00:00:00.000Z&nights=2&isReserving=true&equipmentId=-32768&subEquipmentId=-32768&partySize=2&searchTime=2020-07-31T00:00:00.000Z&resourceLocationId=-2147483594`,
+      
+      // Northeast Washington State Parks
       // site_uri: `/create-booking/results?mapId=-2147483347&searchTabGroupId=0&bookingCategoryId=0&startDate=2020-08-21T00:00:00.000Z&endDate=2020-08-23T00:00:00.000Z&nights=2&isReserving=true&equipmentId=-32768&subEquipmentId=-32768&partySize=2&searchTime=Sat%20Aug%2001%202020%2017:00:00%20GMT-0700%20(Pacific%20Daylight%20Time)`,
       globals_path : 'globals.js',
     }
